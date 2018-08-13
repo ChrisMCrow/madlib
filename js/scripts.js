c9c67abf465242caa46579e534dd5faeef9fba5a
Guest User <Guest@epicodus-4.local>
2018-08-13 10:42:53 -0700

    update code to use .forEach for DRY code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var animalInput= $("input#animal").val();
-    var foodInput = $("input#food").val();
-    var numberInput = $("input#number").val();
-    var adjectiveInput = $("input#adjective").val();
-    var nounPluralInput = $("input#nounplural").val();
-    var nounInput = $("input#noun").val();
-    var partOfBodyInput = $("input#partofbody").val()
+    var blanks = ["animal", "food", "number", "adjective", "nounplural", "noun", "partofbody"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    $(".animal").text(animalInput);
-    $(".food").text(foodInput);
-    $(".number").text(numberInput);
-    $(".adjective").text(adjectiveInput);
-    $(".nounplural").text(nounPluralInput);
-    $(".noun").text(nounInput);
-    $(".partofbody").text(partOfBodyInput);
+
+    // Old code from week 2:
+    // var animalInput= $("input#animal").val();
+    // var foodInput = $("input#food").val();
+    // var numberInput = $("input#number").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var nounPluralInput = $("input#nounplural").val();
+    // var nounInput = $("input#noun").val();
+    // var partOfBodyInput = $("input#partofbody").val()
+    //
+    // $(".animal").text(animalInput);
+    // $(".food").text(foodInput);
+    // $(".number").text(numberInput);
+    // $(".adjective").text(adjectiveInput);
+    // $(".nounplural").text(nounPluralInput);
+    // $(".noun").text(nounInput);
+    // $(".partofbody").text(partOfBodyInput);
 
     $("#story").show();
 
